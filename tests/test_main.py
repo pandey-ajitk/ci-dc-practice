f513982fd9fc099d5549c0ca2407b1e6d49975e0
Ajit Pandey <pandey.ajitk@zohomail.in>
2026-01-12 14:20:13 +0530

    added function and unit test

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from src.main import add,subtract
+
+def test_add():
+    assert add(1,2) == 3
+    assert add(1,0) == 1
+
+def test_subtract():
+    assert subtract(3,2) == 1
+    assert subtract(1,0) == 1
